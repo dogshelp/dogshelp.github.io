ca13eec5c82aab1645bef0952ea8736d1875c054
dogshelp <98406022+dogshelp@users.noreply.github.com>
2022-01-27 16:32:59 +0300

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,17 @@
-$(function() {
-  console.log('start');
+$(() => {
+  $(window).scroll(() => {
+					//Collapse the top bar on scroll
+					if ($("#main-nav").offset().top > 60) {
+						$('.top-bar').slideUp({
+							duration: 250,
+							easing: "easeInOutSine"
+						}).fadeOut(120);
+					} else {
+						$('.top-bar').slideDown({
+							duration: 0,
+							easing: "easeInOutSine"
+						}).fadeIn(120);
+					}
+				});
+
 });
