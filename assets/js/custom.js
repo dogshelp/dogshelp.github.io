72292b55642108de7c17313a0649c3c5a70ba1e1
dogshelp <98406022+dogshelp@users.noreply.github.com>
2022-01-27 17:47:32 +0300

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,19 +1,17 @@
 $(() => {
   $(window).scroll(() => {
-					//Collapse the top bar on scroll
-	  console.log('!!!!!', $("#main-nav").offset().top);
-					/*if ($("#main-nav").offset().top > 60) {
-						
-						$('.top-bar').slideUp({
-							duration: 250,
-							easing: "easeInOutSine"
-						}).fadeOut(120);
-					} else {
-						$('.top-bar').slideDown({
-							duration: 0,
-							easing: "easeInOutSine"
-						}).fadeIn(120);
-					}*/
-				});
+		//Collapse the top bar on scroll
+		if ($("#main-nav").offset().top > 60) {
+			$('.top-bar').slideUp({
+				duration: 250,
+				easing: "easeInOutSine"
+			}).fadeOut(120);
+		} else {
+			$('.top-bar').slideDown({
+				duration: 0,
+				easing: "easeInOutSine"
+			}).fadeIn(120);
+		}
+	});
 
 });
